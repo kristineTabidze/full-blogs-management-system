4c701a024e47ca0a5c6667c076e4600fd5f1c764
kristineTabidze <qristinatabidze@gmail.com>
2021-05-14 18:08:16 +0400

    Add project files.

---
+++ finalProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using finalProject.Models;
+
+namespace finalProject.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<finalProject.Models.Blog> Blog { get; set; }
+    }
+}
---
+++ finalProject/Models/Blog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace finalProject.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string title { get; set; }
+        public string shortDescription { get; set; }
+
+        public Blog ()
+        {
+
+        }
+    }
+}
